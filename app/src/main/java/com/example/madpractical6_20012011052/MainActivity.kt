7ed1601b47b5faf60012a168b55e3c6b1ffb5e48
hetkikani <hetkikani20@gnu.ac.in>
2022-11-10 22:45:32 +0530

    prac6

---
+++ app/src/main/java/com/example/madpractical6_20012011052/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.madpractical6_20012011052
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import com.example.madpractical6_20012011052.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVideoPlayer()
+        binding.youtubeButton.setOnClickListener{
+            Intent(this,YoutubeActivity::class.java).apply { startActivity(this) }
+        }
+    }
+    private fun initVideoPlayer(){
+        val mediaController=MediaController(this)
+        var uri:Uri=Uri.parse("android.resource://"+packageName+"/"+R.raw.thestoryoflight)
+        binding.myVideoView.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.myVideoView)
+        binding.myVideoView.setVideoURI(uri)
+        binding.myVideoView.requestFocus()
+        binding.myVideoView.start()
+    }
+}
